e9d756ada9b15fdd476f615494696384ce3f8dde
ABaykov <alexandr.baykov@gmail.com>
2018-12-27 15:04:04 +0300

    Added spring-mvc

--- src/main/java/ru/javawebinar/graduation/web/VoteController.java
+++ src/main/java/ru/javawebinar/graduation/web/VoteController.java
@@ -1,11 +1,13 @@
 package ru.javawebinar.graduation.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 import ru.javawebinar.graduation.model.Vote;
 import ru.javawebinar.graduation.model.VotePk;
 import ru.javawebinar.graduation.repository.RestaurantRepository;
 import ru.javawebinar.graduation.repository.VoteRepository;
 
+@RestController
 public class VoteController {
 
     @Autowired
